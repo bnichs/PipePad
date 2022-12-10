08207c62a69f396ea057d87ab17a02ff4aa81d4f
Ben <mail@ben-nichols.net>
2022-12-10 15:18:03 -0800

    Init working

--- pypad.py
+++ pypad.py
@@ -16,28 +16,62 @@ EDITOR = "vim "
 
 initial_message = 'hello there'  # if you want to set up the file somehow
 
-with tempfile.NamedTemporaryFile(suffix=".tmp") as tf:
-    tf.write(initial_message.encode('utf-8'))
 
-    tf.flush()
-    args = EDITOR.split() + [tf.name]
+# def setNonBlocking(fd):
+#     """
+#     Set the file description of the given file descriptor to non-blocking.
+#     """
+#     flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+#     flags = flags | os.O_NONBLOCK
+#     fcntl.fcntl(fd, fcntl.F_SETFL, flags)
+
+def run_editor(py_file):
+    # with tempfile.NamedTemporaryFile(suffix=".tmp") as tf:
+    py_file.write(initial_message.encode('utf-8'))
+    py_file.flush()
+
+
+    use_tty = sys.stdin.isatty() and not sys.stdout.isatty()
+    if use_tty:
+        stdout = open("/dev/tty", 'wb')
+    else:
+        stdout = sys.stdout
+
+    args = EDITOR.split() + [py_file.name]
     print(args)
-    subprocess.run(args, shell=False, stdin=subprocess.PIPE)
-
-
+    subprocess.run(args, shell=False, text=True, stdin=subprocess.PIPE, close_fds=True)
 
     # do the parsing with `tf` using regular File operations.
     # for instance:
-    tf.seek(0)
-    edited_message = tf.read()
+    py_file.seek(0)
+    edited_message = py_file.read()
+
+    # curses.setupterm()
+    # sys.stdout.write(curses.tigetstr('sgr0').decode("utf-8"))
+
+    os.system('stty ^U')
+    os.system('reset')
 
     # subprocess.run(["clear"], shell=True)
-    os.system("stty sane")
+    # os.system("stty sane")
 
     print("Output:")
     print(edited_message)
-    for i in range(100):
+    for i in range(5):
         print(i)
 
+    print("From stdin")
+    for el in sys.stdin:
+        print(el)
 
+
+def run():
+    with tempfile.NamedTemporaryFile(suffix=".tmp") as tf:
+        run_editor(tf)
+
+
+
+# run_1()
+
+run()
 
