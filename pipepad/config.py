bdf646b4f8d7a90d5a51360ef63ed79d3deaf0a2
Ben <mail@ben-nichols.net>
2022-12-15 12:34:26 -0800

    Config comments

--- pipepad/config.py
+++ pipepad/config.py
@@ -1,32 +1,34 @@
 import os
 
+# Primitives
 APP_NAME = "pipepad"
+PAD_EXTENSION = "ppad"
 
 
+# Package locations
 MODULE_DIR = os.path.dirname(os.path.realpath(__file__))
 PACKAGE_DIR = os.path.dirname(MODULE_DIR)
+BIN_PATH = os.path.join(PACKAGE_DIR, "bin")
 
 
+# Templates
 TEMPLATE_DIR = os.path.join(PACKAGE_DIR, "pipepad", "templates")
-
 TEMPLATE_FILE = "template"
 
 # TODO for python only
 TEMPLATE_FILE_PY = "template.ppad.py"
 PY_TEMPLATE_PATH = os.path.join(TEMPLATE_DIR, TEMPLATE_FILE_PY)
 
-BIN_PATH = os.path.join(PACKAGE_DIR, "bin")
 
-
+# Todo lang agnostic
 RUN_PAD_PATH = os.path.join(BIN_PATH, "run_pad.sh")
 
 
 DEFAULT_REPO = "local"
 
 
-PAD_EXTENSION = "ppad"
 
-
+# Headers
 # We use tokens because not every language has the same comments so we need to know where to find the data
 HEADER_START_STRING = "START_HEADER"
 HEADER_END_STRING = "END_HEADER"
@@ -38,7 +40,7 @@ HEADER_LANG_KEY = "language"
 HEADER_HASH_KEY = "hash"
 
 
-
+# Pads
 # How many characters for a short hash
 SHORT_HASH_LENGTH = 8
 
