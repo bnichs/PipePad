c38de2178d7c7189b341907007d87b6666790c75
Ben <mail@ben-nichols.net>
2022-12-15 21:13:37 -0800

    Settings attempts

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,18 +1,32 @@
+import os
 from pprint import pprint
 
+import pytest
+
 from pipepad.config import settings
+from tests.util import dummy_str
 
 
 def test_show():
     pprint(settings.as_dict())
-
-    raise
 
 
 def test_simple_get():
     assert settings.pipepad.default_language == "python"
 
 
-def test_override():
-    settings.configure()
-    pass
+def test_override(tmp_path, request):
+    with pytest.raises(AttributeError):
+        assert settings.foo == "baz"
+
+    id = dummy_str()
+    name = request.node.name
+    fpath = os.path.join(tmp_path, f"{name}.toml")
+
+    with open(fpath, 'w') as f:
+        f.write(f'[ppad]\nfoo = "{id}"')
+
+
+    settings.load_file(fpath, silent=False)
+
+    assert settings.ppad.foo == id
