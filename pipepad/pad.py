6639596ce848354ac1d0b755c53c400ddcbb04be
Ben <mail@ben-nichols.net>
2022-12-16 16:17:12 -0800

    Get PadIDs working

--- pipepad/pad.py
+++ pipepad/pad.py
@@ -1,8 +1,11 @@
 import hashlib
 import logging
+import os
 from dataclasses import dataclass
 from os import PathLike
-from typing import Self
+from pathlib import Path
+from typing import Self, Tuple
+from urllib.parse import urlparse, ParseResult
 
 from pipepad.config_old import SHORT_HASH_LENGTH, LATEST_VERSION
 from pipepad.language import PadLanguage
@@ -13,6 +16,10 @@ logger = logging.getLogger(__name__)
 
 
 """
+Basic format: 
+
+$REPO_ID:$PAD_NAME:$VERSION
+
 Valid pad ids:
 
 * samples/sum_ints  = the sum_ints pad in the samples repo
@@ -25,9 +32,52 @@ Valid pad ids:
 """
 
 
+class CantParseID(Exception):
+    pass
+
+
+class InvalidRemoteID(Exception):
+    pass
+
+
+def path_exists(path: PathLike):
+    return os.path.exists(path) or os.path.islink(path)
+
+
+def parse_version(vstr: str) -> int:
+    if vstr == "latest":
+        return LATEST_VERSION
+    else:
+        return int(vstr)
+
+
+
 @dataclass
 class RepoID:
     pass
+
+
+@dataclass
+class RemoteRepoID():
+    scheme: str
+    netloc: str
+    path: str
+
+    @classmethod
+    def from_parsed(cls, parsed: ParseResult):
+        logger.debug("Making remote repo id from %s", parsed)
+
+        parts = parsed.path.split(":")
+        match parts:
+            case (repo_part, pad_part):
+                return RemoteRepoID(scheme=parsed.scheme, netloc=parsed.netloc, path=repo_part)
+            case (repo_part, pad_part, version):
+                return RemoteRepoID(scheme=parsed.scheme, netloc=parsed.netloc, path=repo_part)
+            case (filepath,):
+                return RemoteRepoID(scheme=parsed.scheme, netloc=parsed.netloc, path=None)
+            case _:
+                print(parts)
+                raise
 
 
 # @dataclass
@@ -35,10 +85,42 @@ class PadID:
     @classmethod
     def from_str(cls, s: str) -> Self:
         logger.debug("Making PadID from %s", s)
+        parsed = urlparse(s)
+        print(parsed)
 
+        if parsed.scheme == "file" or path_exists(Path(s)):
+            logger.debug("Looks like a file, parsing as file")
+            return LocalFilePadID.from_str(s)
+        elif parsed.scheme in ["http", "https", "git"]:
+            logger.debug("Looks like a remote repository")
+            return RemotePadID.from_str(s)
+        else:
+            logger.debug("Looks like a local repo id")
+            return RepoPadID.from_str(s)
 
-        raise
-        pass
+        return "foo"
+
+    def is_file(self):
+        return isinstance(self, LocalFilePadID)
+
+    def is_remote(self):
+        return isinstance(self, RemotePadID)
+
+    @classmethod
+    def parse_path(cls, s: str) -> Tuple[str, str, int]:
+        parts = s.split(":")
+        logger.debug("Parts = %s", parts)
+
+        match parts:
+            case (repo_name, pad_name, version):
+                version = parse_version(version)
+                return repo_name, pad_name, version
+            case (repo_name, pad_name):
+                return repo_name, pad_name, LATEST_VERSION
+            case (file_path, ):
+                return None, file_path, None
+            case _:
+                raise CantParseID(s)
 
 
 @dataclass
@@ -48,11 +130,76 @@ class RepoPadID(PadID):
     pad_name: str
     version: int = LATEST_VERSION
 
+    @classmethod
+    def from_str(cls, s: str) -> Self:
+        logger.debug("Making repo+pad id from %s", s)
 
-class FilePadID(PadID):
+        repo_name, pad_name, version = cls.parse_path(s)
+        return RepoPadID(repo_name, pad_name, version)
+
+
+@dataclass
+class RemotePadID(PadID):
+    repo: RemoteRepoID
+    name: str
+    version: int = LATEST_VERSION
+
+    @classmethod
+    def from_str(cls, s: str) -> Self:
+        logger.debug("making remote pad id from %s", s)
+        parsed = urlparse(s)
+        print(parsed)
+        if not parsed.scheme:
+            raise InvalidRemoteID(s)
+
+        repo_name, pad_name, version = cls.parse_path(parsed.path)
+
+        if repo_name is None and version is None:
+            # Nothing parsed so it ust be a file path
+            return RemotePadFileID.from_parsed(filepath=pad_name, parsed=parsed)
+            pass
+        else:
+            # we at least got a pad name
+            repo_name = ...
+            print(type(parsed))
+            repo = RemoteRepoID.from_parsed(parsed)
+            return RemotePadID(repo=repo, name=pad_name, version=version)
+            pass
+
+        raise
+
+    # @classmethod
+    # def parse_path(cls, p: str):
+    #     pass
+
+
+@dataclass
+class RemotePadFileID(RemotePadID):
+    """A remote tht is just a file, not necessarily in a repo"""
+    path: PathLike = None
+
+    @classmethod
+    def from_parsed(cls, filepath: PathLike, parsed: ParseResult):
+        repo = RemoteRepoID.from_parsed(parsed)
+        print(parsed)
+        print(repo)
+        return RemotePadFileID(repo=repo, name=None, version=None, path=filepath)
+
+
+@dataclass
+class LocalFilePadID(PadID):
     """A PadID that is just a file path"""
     fpath: PathLike
 
+    @classmethod
+    def from_str(cls, s: str) -> Self:
+        logger.debug("Making file pad id from %s", s)
+        if s.startswith("file://"):
+            s = s.replace("file://", "")
+        return LocalFilePadID(Path(s))
+
+    def file_exists(self):
+        return path_exists(self.fpath)
 
 
 
--- tests/test_pad.py
+++ tests/test_pad.py
@@ -1,36 +1,80 @@
+from pipepad.config_old import LATEST_VERSION
 from pipepad.pad import PadID
 
 
 
 
 class TestPadID:
-    # def test_file_path(self):
-    #     pad_id = PadID.from_str("file://./samples/sum_ints.pad.py")
-
     def test_simple_pad_id(self):
-        pad_id = "samples/sum_ints"
+        pad_id = "samples:sum_ints"
         pad_id_out = PadID.from_str(pad_id)
+
+        assert pad_id_out.pad_name == "sum_ints"
+        assert pad_id_out.repo == "samples"
+        assert pad_id_out.version == LATEST_VERSION
 
     def test_simple_pad_id_with_version(self):
-        pad_id = "samples/sum_ints:5"
+        pad_id = "samples:sum_ints:5"
         pad_id_out = PadID.from_str(pad_id)
+        print(pad_id_out)
+
+        assert pad_id_out.pad_name == "sum_ints"
+        assert pad_id_out.repo == "samples"
+        assert pad_id_out.version == 5
+
+    def test_simple_pad_id_with_version_str(self):
+        pad_id = "samples:sum_ints:latest"
+        pad_id_out = PadID.from_str(pad_id)
+        print(pad_id_out)
+
+        assert pad_id_out.pad_name == "sum_ints"
+        assert pad_id_out.repo == "samples"
+        assert pad_id_out.version == LATEST_VERSION
 
     def test_file_pad_id(self):
         pad_id = "./samples/sample_repo/sum_ints/latest.py"
         pad_id_out = PadID.from_str(pad_id)
+        assert pad_id_out.is_file()
+        assert pad_id_out.file_exists()
+        print(pad_id_out)
 
     def test_file_pad_id_with_schema(self):
         pad_id = "file://./samples/sample_repo/sum_ints/latest.py"
         pad_id_out = PadID.from_str(pad_id)
+        assert pad_id_out.is_file()
+        assert pad_id_out.file_exists()
+        print(pad_id_out)
 
     def test_remote_repo_with_file(self):
-        pad_id = "git@github.com:bnichs/pad_repo/samples/sample_repo/sum_ints/latest.py"
+        pad_id = "git://git@github.com:bnichs/pad_repo/samples/sample_repo/sum_ints/latest.py"
         pad_id_out = PadID.from_str(pad_id)
+
+        assert pad_id_out.is_remote()
+
+        assert pad_id_out.repo.scheme == "git"
+        assert pad_id_out.repo.netloc == "git@github.com:bnichs"
+        assert pad_id_out.path == "/pad_repo/samples/sample_repo/sum_ints/latest.py"
 
     def test_remote_repo_with_pad(self):
-        pad_id = "git@github.com:bnichs/pad_repo/samples/sum_ints"
+        pad_id = "git://git@github.com:bnichs/pad_repo/samples:sum_ints"
         pad_id_out = PadID.from_str(pad_id)
+        assert pad_id_out.name == "sum_ints"
+
+        assert pad_id_out.repo.scheme == "git"
+        assert pad_id_out.repo.netloc == "git@github.com:bnichs"
+        assert pad_id_out.repo.path == "/pad_repo/samples"
 
     def test_remote_repo_with_pad_and_version(self):
-        pad_id = "https://github.com/bnichs/pad_repo/samples/sum_ints:5"
+        pad_id = "https://github.com/bnichs/pad_repo/samples:sum_ints:5"
         pad_id_out = PadID.from_str(pad_id)
+        assert pad_id_out.name == "sum_ints"
+
+        assert pad_id_out.repo.scheme == "https"
+        assert pad_id_out.repo.netloc == "github.com"
+        assert pad_id_out.repo.path == "/bnichs/pad_repo/samples"
+
+        print(pad_id_out)
+
+    # @classmethod
+    # def teardown_class(cls):
+    #     raise
